1cdb2d559a64fa049d648e033f1df368a889f63a
Gabriel Ferns <exclamaforte@gmail.com>
2014-11-22 15:20:04 -0600

    Adding random changes

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -18,5 +18,20 @@
   (unless (eq system-type 'windows-nt)
    (set-selection-coding-system 'utf-8))
   (prefer-coding-system 'utf-8))
+;;;if package is not working
+;;; M-x package-refresh-contents
+(blink-cursor-mode 0)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
+(scroll-bar-mode -1)
+(set slime-backend "/home/gabe/.emacs.d/elpa/slime-20141006.1500/swank-loader.lisp")
 
+(global-set-key [f5] 'slime-js-reload)
+(add-hook 'js2-mode-hook
+          (lambda ()
+            (slime-js-minor-mode 1)))
+(add-hook 'css-mode-hook
+          (lambda ()
+            (define-key css-mode-map "\M-\C-x" 'slime-js-refresh-css)
+            (define-key css-mode-map "\C-c\C-r" 'slime-js-embed-css)))
 (provide 'init-locales)
