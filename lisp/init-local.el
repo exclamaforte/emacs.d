4e1ce62d8b32fd2845c5bd6ee6c130a473056290
Gabriel Ferns <exclamaforte@gmail.com>
2015-07-22 21:57:25 -0500

    flyhook stuff

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -96,3 +96,7 @@
 (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
 ;;; init-local.el ends here
 (set-face-attribute 'default nil :height 117)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(eval-after-load 'flycheck
+  '(setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
